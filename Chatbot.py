40870016fbacd7c8010b218d69820d39b7a97e04
Bhanu Prasad <mudavathbhanu5@gmail.com>
2026-06-21 00:25:17 +0530

    Updated chatbot and fixed responses

--- Chatbot.py
+++ Chatbot.py
@@ -1,39 +1,58 @@
 import datetime
-print ("Wlcome to Basic chat Box")
-while True: 
-    user=input("User:")
-    if user.lower()=="hi":
-        print("Bot : HEllo")
-    elif user.lower()=="hello":
+
+print("Welcome to Basic Chat Bot")
+
+while True:
+    user = input("User: ")
+
+    if user.lower() == "hi":
+        print("Bot : Hello")
+
+    elif user.lower() == "hello":
         print("Bot : Hiiii")
-    elif user.lower()=="how are you":
-        print("Bot : Im Great,what about you")
-    elif user.lower()=="fine" or user.lower()== "great" or user.lower()=="good":
-        print("Bot : That Awsome")
-    elif user.lower()=="what is your name":
+
+    elif user.lower() == "how are you":
+        print("Bot : I'm Great, what about you?")
+
+    elif user.lower() == "fine" or user.lower() == "great" or user.lower() == "good":
+        print("Bot : That's Awesome!")
+
+    elif user.lower() == "what is your name":
         print("Bot : My name is MINIBOT")
-    elif user.lower()== "time":
-        currenttime=datetime.datetime.now().strftime("%H:%M:%S")
-        print("Bot : ",currenttime)
-    elif user.lower()=="date":
-        currentdate=datetime.datetime.now().strftime("%d/%m/%Y")
-        print("Bot : ",currentdate)
-    elif user.lower()=="what is your fav language":
-        print("Bot :Ofourse its Python")
-    elif user.lower()=="thank you":
-        print("Bot : Welcom")
-    elif user.lower()=="who created you":
-        print("Bot : I was created using Python")
+
+    elif user.lower() == "time":
+        currenttime = datetime.datetime.now().strftime("%H:%M:%S")
+        print("Bot :", currenttime)
+
+    elif user.lower() == "date":
+        currentdate = datetime.datetime.now().strftime("%d/%m/%Y")
+        print("Bot :", currentdate)
+
+    elif user.lower() == "what is your fav language":
+        print("Bot : Of course, it's Python!")
+
+    elif user.lower() == "thank you":
+        print("Bot : You're Welcome!")
+
+    elif user.lower() == "who created you":
+        print("Bot : I was created using Python.")
+
+    elif user.lower() == "joke":
+        print("Bot : Why do programmers prefer dark mode? Because light attracts bugs!")
+
     elif user.lower() == "help":
-        print("Bot: Try these commands:")
+        print("Bot : Try these commands:")
         print("hello")
         print("time")
         print("date")
         print("what is your name")
+        print("who created you")
+        print("joke")
         print("bye")
-        
-    elif user.lower()=="bye":
-        print("Bot : GoodBye")  
+
+    elif user.lower() == "bye":
+        print("Bot : Goodbye!")
         break
+
     else:
-        print("Bot : Did'nt undertood!!!!")
+        print("Bot : Didn't understand!!!!")
